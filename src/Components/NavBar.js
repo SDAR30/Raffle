aca28740f102bbc0287a3b8c427e01839931b1c6
Shoaib Dar <SDAR30@gmail.com>
2021-05-31 15:46:08 -0400

    filter participants feature added

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -24,14 +24,16 @@ const useStyles = makeStyles({
 const NavBar = () => {
     const classes = useStyles();
     const { id } = useParams();
-    return (<nav><AppBar position="static" color="default" height="100px" className={classes.NavHeader}>
-        <Toolbar>
-            <Typography varient="h1" className={classes.root}> <NavLink className={classes.NavStyle} to="/" exact={true}>All Raffles</NavLink></Typography>
-            <Typography varient="h4" className={classes.root}> <NavLink className={classes.NavStyle} to={`/raffles/${id}`} exact={true}>Register</NavLink></Typography>
-            <Typography varient="h4" className={classes.root}>  <NavLink className={classes.NavStyle} to={`/raffles/${id}/participants`} exact={true}>Participants</NavLink></Typography>
-            <Typography varient="h4" className={classes.root}>  <NavLink className={classes.NavStyle} to={`/raffles/${id}/winner`} exact={true}>Pick Winner</NavLink></Typography>
-        </Toolbar>
-    </AppBar></nav>)
+    return (<nav>
+        <AppBar position="static" color="default" height="100px" className={classes.NavHeader}>
+            <Toolbar>
+                <Typography varient="h1" className={classes.root}> <NavLink className={classes.NavStyle} to="/" exact={true}>All Raffles</NavLink></Typography>
+                <Typography varient="h4" className={classes.root}> <NavLink className={classes.NavStyle} to={`/raffles/${id}`} exact={true}>Register</NavLink></Typography>
+                <Typography varient="h4" className={classes.root}>  <NavLink className={classes.NavStyle} to={`/raffles/${id}/participants`} exact={true}>Participants</NavLink></Typography>
+                <Typography varient="h4" className={classes.root}>  <NavLink className={classes.NavStyle} to={`/raffles/${id}/winner`} exact={true}>Pick Winner</NavLink></Typography>
+            </Toolbar>
+        </AppBar>
+    </nav>)
 }
 
 export default NavBar;
--- src/Components/Participants.js
+++ src/Components/Participants.js
@@ -1,17 +1,28 @@
 import NavBar from "./NavBar"
 import { useEffect, useState } from "react";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom"
 import axios from "axios"
 import ParticipantListItem from "./ParticipantListItem";
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
-const Participants = (props) =>{
-    const [participantList,setParticipantList] = useState([]);
+const useStyles = makeStyles((theme) => ({
+    filterInput: {
+        margin: theme.spacing(0),
+        width: '100ch',
+    },
+}));
+
+const Participants = (props) => {
+    const classes = useStyles();
+    const [participantList, setParticipantList] = useState([]);
+    const [filter, setFilter] = useState("");
     const { id } = useParams();
 
-    const retrieveParticipants = async ()=>{
+    const retrieveParticipants = async () => {
         try {
-            const {data} = await axios.get(`/api/raffles/${id}/participants`);
-            
+            const { data } = await axios.get(`/api/raffles/${id}/participants`);
+
             setParticipantList(data)
         } catch (error) {
             setParticipantList([])
@@ -19,18 +30,24 @@ const Participants = (props) =>{
         }
     }
 
-    useEffect(()=>{
+    const handleFilter = (e) => setFilter(e.target.value)
+
+    useEffect(() => {
         retrieveParticipants()
 
-    },[])
+    }, [])
 
+    const matches = participantList.filter(participant => participant.firstname.toLowerCase().includes(filter.toLowerCase()) || participant.lastname.toLowerCase().includes(filter.toLowerCase()))
+        .map(participant => <ParticipantListItem key={participant.id} participant={participant} />)
 
-    return(<div>
-  
-        <NavBar/>
+    return (<div>
+
+        <NavBar />
         <h2 className="title-header">{props.title}</h2>
+        <h2 className="total-participants" style={{ textAlign: 'left', marginLeft: "50px" }}>Participants: {matches.length} total</h2>
+        <TextField id="outlined-basic" className={classes.filterInput} label="Search..." variant="outlined" value={filter} onChange={handleFilter} />
         <ul>
-            {participantList.map(participant=> <ParticipantListItem key={participant.id} participant={participant}/>)}
+            {matches}
         </ul>
     </div>)
 
